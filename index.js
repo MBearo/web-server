d54478b4fc6d62eb5044699cf3e226b8b95ec370
MBearo <295290708@qq.com>
2019-09-19 12:37:55 +0800

    UPG:add comment

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const serve = require('koa-static')
 const Koa = require('koa')
 const app = new Koa()
 // const order = ['-port', '-path', 'list']
-// 获取命令的参数
+
+// Get parameters
 const args = process.argv.slice(2)
-console.log(args)
-console.log(`pid is ${process.pid}`)
+// console.log(args)
+// console.log(`pid is ${process.pid}`)
 
 let staticPath = '.'
 let port = '3000'
 
 checkArgument().then(_ => {
   app.use(serve(path.resolve(staticPath)))
-
   app.listen(port)
-
   console.log(`web-server start on http://localhost:${port}`)
 })
 
@@ -29,7 +28,7 @@ async function checkArgument () {
         await portIsOccupied(args[i + 1])
         port = args[i + 1]
       } else {
-        // throw new Error("can't find '-port' arguments !")
+        throw new Error("can't find '-port' parameters !")
       }
     } else if (args[i] === '-path') {
       if (args[i + 1]) {
@@ -38,19 +37,18 @@ async function checkArgument () {
     }
   }
 }
-// 检测端口是否被占用
+
+// Check if the port is occupied
 function portIsOccupied (port) {
   return new Promise((resolve, reject) => {
-    // 创建服务并监听该端口
+    // Create server and listen this port
     let server = net.createServer().listen(port)
-
-    server.on('listening', _ => { // 执行这块代码说明端口未被占用
-      server.close() // 关闭服务
+    server.on('listening', _ => { // Executing this code indicates that the port is not occupied
+      server.close() // close server
       resolve()
     })
-
     server.on('error', err => {
-      if (err.code === 'EADDRINUSE') { // 端口已经被使用
+      if (err.code === 'EADDRINUSE') { // The port is occupied
         reject(new Error(`The port '${port}' is occupied, please change other port.`))
         throw new Error(`The port '${port}' is occupied, please change other port.`)
       }
